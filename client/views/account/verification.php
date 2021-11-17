0949d51acddf60ca8cafce6feb96eb532e74d3a2
quantroy <93896090+quantroy@users.noreply.github.com>
2021-11-17 23:07:53 +0700

    Update verification.php

---
+++ client/views/account/verification.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="<?= CLIENT_ASSET ?>css/bootstrap.min.css" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Xiao Haha</title>
+</head>
+
+<body>
+    <div style="max-width: 400px; margin:0 auto; background-color: #ffa400;" class="p-4">
+        <h3>Nhập mã xác nhận</h3>
+        <form action="quen-mat-khau" method="post">
+            <div class="form-group">
+                <label for="email">Email</label><br>
+
+                <!-- Php -->
+                <?php if (isset($error['email'])) { ?>
+                    <span class='text-danger'><?= $error['email'] ?></span>
+                    <?php $error = []; ?>
+                <?php } ?>
+
+                <input type="email" class="form-control" name="email" id="email" placeholder="Nhập mã xác nhận">
+            </div>
+            <input class="btn btn-info" type="submit" name="submit" value="Gửi">
+        </form>
+    </div>
+</body>
+
+</html>
