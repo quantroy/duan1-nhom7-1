f4d3c7e5131f63455ce3c60a57ab6251e0908615
quantroy <93896090+quantroy@users.noreply.github.com>
2021-11-21 23:29:00 +0700

    sua giao dien quen mat khau

--- client/views/account/forgot_password.php
+++ client/views/account/forgot_password.php
@@ -1,18 +1,22 @@
-<div style="max-width: 400px; margin:0 auto; background-color: #ffa400;" class="p-4">
-    <h3>Quên mật khẩu</h3>
-    <form action="quen-mat-khau" method="post">
-        <div class="form-group">
-            <label for="email">Email</label><br>
+<section class="featured spad" style="background:#efefef;">
+    <div class="container shadow p-3 mb-5 bg-body rounded " style="background:#fff">
+        <h3 style="font-weight:bold;text-align: center;font-size: 24px;color:#0d713d;margin-bottom: 50px;padding-top: 70px;">Quên mật khẩu</h3>
+        <div class="align-middle" style="text-align:center;">
+            <form class="w-75" style="display: inline-block;text-align: left;padding: 45px 250px;" action="<?= BASE_URL ?>tai-khoan/quen-mat-khau" method="post">
+                <div class="form-group">
+                    <div> <label style="font-weight: bold" for="">Email</label> </div>
 
-            <!-- Php -->
-            <?php if (isset($_SESSION['false'])) { ?>
-                <span class='text-danger'><?= $_SESSION['false'] ?></span>
+                    <?php if (isset($_SESSION['false'])) { ?>
+                        <span class='text-danger'><?= $_SESSION['false'] ?></span>
 
-            <?php } ?>
-            <input type="email" class="form-control" name="email" id="email" placeholder="Nhập email của bạn">
+                    <?php } ?>
+                    <input class="form-control " type="email" name="email" id="" value placeholder="Email">
+                </div>
+
+
+                <input style="margin-left:75px;margin-top:30px;background:#0d713d;" class="btn btn-success " type="submit" name="submit" value="Gửi yêu cầu">
+                <button style="margin-top:30px;background:#0d713d;" class="btn btn-success " type="reset">Nhập Lại</button>
+            </form>
         </div>
-        <input class="btn btn-info" type="submit" name="submit" value="Gửi yêu cầu">
-    </form>
-</div>
-
-<?php require_once './client/business/account.php'; ?>
+    </div>
+</section>
--- client/views/account/verification.php
+++ client/views/account/verification.php
@@ -1,19 +1,26 @@
-<div style="max-width: 400px; margin:0 auto; background-color: #ffa400;" class="p-4">
-    <form action="kiem-tra-ma" method="post">
-        <h3>Nhập mã xác nhận</h3>
-        <div class="form-group">
+<section class="featured spad" style="background:#efefef;">
+    <div class="container shadow p-3 mb-5 bg-body rounded " style="background:#fff">
+        <h3 style="font-weight:bold;text-align: center;font-size: 24px;color:#0d713d;margin-bottom: 50px;padding-top: 70px;">Nhập mã xác nhận</h3>
+        <div class="align-middle" style="text-align:center;">
+            <form class="w-75" style="display: inline-block;text-align: left;padding: 45px 250px;" action="<?= BASE_URL ?>tai-khoan/kiem-tra-ma" method="post">
 
-            <!-- Php -->
-            <?php if (isset($_SESSION['false'])) : ?>
-                <div class='alert alert-danger' role="alert">
-                    <?= $_SESSION['false'] ?>
+                <!-- Php -->
+                <?php if (isset($_SESSION['false'])) : ?>
+                    <div class='alert alert-danger' role="alert">
+                        <?= $_SESSION['false'] ?>
+                    </div>
+                <?php else :  ?>
+                    <div class='alert alert-primary' role="alert">Nhập mã xác nhận chúng tôi đã gửi cho bạn về Email</div>
+                <?php endif ?>
+                <div class="form-group">
+
+                    <input class="form-control " type="text" name="code" value placeholder="Nhập mã xác nhận">
                 </div>
-            <?php else :  ?>
-                <div class='alert alert-primary' role="alert">Nhập mã xác nhận chúng tôi đã gửi cho bạn về Email</div>
-            <?php endif ?>
 
-            <input type="text" class="form-control" name="code" placeholder="Nhập mã xác nhận">
+
+                <input style="margin-left:75px;margin-top:30px;background:#0d713d;" class="btn btn-success " type="submit" name="submit" value="Gửi">
+                <button style="margin-top:30px;background:#0d713d;" class="btn btn-success " type="reset">Nhập Lại</button>
+            </form>
         </div>
-        <input class="btn btn-info" type="submit" name="submit" value="Gửi">
-    </form>
-</div>
+    </div>
+</section>
