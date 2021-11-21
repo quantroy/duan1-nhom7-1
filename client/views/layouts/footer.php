5fffad1f7bef92e792be8d1c10bf0aa6889eee9d
quantroy <93896090+quantroy@users.noreply.github.com>
2021-11-21 13:36:28 +0700

    update footer

--- client/views/layouts/footer.php
+++ client/views/layouts/footer.php
@@ -19,7 +19,6 @@
                     <ul>
                         <li><a href="#">Về chúng tôi</a></li>
                         <li><a href="#">Liên hệ</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
                         <li><a href="#">Chính sách bảo mật</a></li>
                         <li><a href="#">Chính sách đổi trả</a></li>
                     </ul>
