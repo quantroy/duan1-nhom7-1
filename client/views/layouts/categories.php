3604b5bfc788456278dd8906a60a211ae231fa8e
quantroy <93896090+quantroy@users.noreply.github.com>
2021-11-29 00:50:08 +0700

    a

--- client/views/layouts/categories.php
+++ client/views/layouts/categories.php
@@ -2,13 +2,13 @@
     <div class="container">
         <div class="row">
             <div class="categories__slider owl-carousel">
-            <?php  $top10 = loadall_sanpham_top10() ?>
-                <?php foreach($top10 as $top):?>
+                <?php $top10 = loadall_sanpham_top10() ?>
+                <?php foreach ($top10 as $top) : ?>
                     <div class="col-lg-3">
-                <div class="categories__item set-bg" data-setbg="<?= $top['thumbnail'] ?>">        
-                        <h5><a href="#"><?= $top['name'] ?></a></h5>
-                </div>
-                </div>
+                        <div class="categories__item set-bg" data-setbg="<?= $top['thumbnail'] ?>">
+                            <h5><a href="#"><?= $top['name'] ?></a></h5>
+                        </div>
+                    </div>
                 <?php endforeach ?>
             </div>
         </div>
