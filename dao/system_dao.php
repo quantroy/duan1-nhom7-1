52fb2766ffbc3c46b466b3987bc72c93ed92d590
quan08 <93896690+quan08@users.noreply.github.com>
2021-11-29 09:38:51 +0700

    dat hang

--- dao/system_dao.php
+++ dao/system_dao.php
@@ -1,7 +1,7 @@
 <?php
 function pdo_get_connection()
 {
-    $dburl = "mysql:host=localhost;dbname=2021-duan1-nhom7;charset=utf8";
+    $dburl = "mysql:host=localhost;dbname=2021-duan1;charset=utf8";
     $username = 'root';
     $password = '';
 
