d5351402f2edfc00b4f8c6c94e27f6c2a149a1ec
quantroy <93896090+quantroy@users.noreply.github.com>
2021-11-30 15:21:02 +0700

    add contact

--- client/business/contact.php
+++ client/business/contact.php
@@ -1,8 +1,6 @@
 <?php
 function contact()
 {
-    $sql = "SELECT * FROM  contact";
-    $contact = pdo_execute($sql);
-    client_render('contact/index.php', compact('contact'));
+    
+    client_render('contact/index.php');
 }
-?>
