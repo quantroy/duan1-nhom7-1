e6cf04455f79b5cd5458b804c06647d5d1f64ace
nbtuyen <tuyennbph13946@fpt.edu.vn>
2021-12-02 19:35:17 +0700

    Update blog.php

--- client/views/blog/blog.php
+++ client/views/blog/blog.php
@@ -45,7 +45,7 @@
                                     <div class="blog__sidebar__recent__item__pic" style="width:100px">
                                         <img src="<?= $p['thumbnail1'] ?>" alt="">
                                     </div>
-                                    <div class="blog__sidebar__recent__item__text" style="width:240px;margin-top:20px;">
+                                    <div class="blog__sidebar__recent__item__text" style="width:240px;">
                                         <h6><?= $p['title'] ?></h6>
                                         <span><?= $p['created_at'] ?></span>
                                     </div>
@@ -58,13 +58,21 @@
             <?php foreach ($post as $post) : ?>
                 <div class="col-lg-8 col-md-7 order-md-1 order-1">
                     <div class="blog__details__text">
-                        <img src="<?= $post['thumbnail1'] ?>" alt="" style="width:70%">
-                        <p><?= $post['content'] ?></p>
                         <h3><?= $post['title'] ?></h3>
+                        <p><?= $post['content'] ?></p>
+                        <img src="<?= $post['thumbnail1'] ?>" alt="" style="width:70%">
                         <p><?= $post['content'] ?></p>
                     </div>
                     <div class="blog__details__content">
                         <div class="row">
+                            <div class="col-lg-6">
+                                <div class="blog__details__author">
+                                    <div class="blog__details__author__text" style="margin-top: -45px;">
+                                        <h6>Tác Giả:<?= $post['created_by'] ?></h6>
+                                        <span>Ngày Đăng:<?= $post['created_at'] ?></span>
+                                    </div>
+                                </div>
+                            </div>
                             <div class="col-lg-6">
                                 <div class="blog__details__widget">
                                     <div class="blog__details__social">
@@ -84,63 +92,3 @@
     </div>
 </section>
 <!-- Blog Details Section End -->
-
-<!-- Related Blog Section Begin -->
-<section class="related-blog spad">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="section-title related-blog-title">
-                    <h2>Post You May Like</h2>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-4 col-md-4 col-sm-6">
-                <div class="blog__item">
-                    <div class="blog__item__pic">
-                        <img src="img/blog/blog-1.jpg" alt="">
-                    </div>
-                    <div class="blog__item__text">
-                        <ul>
-                            <li><i class="fa fa-calendar-o"></i> May 4,2019</li>
-                            <li><i class="fa fa-comment-o"></i> 5</li>
-                        </ul>
-                        <h5><a href="#">Cooking tips make cooking simple</a></h5>
-                        <p>Sed quia non numquam modi tempora indunt ut labore et dolore magnam aliquam quaerat </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-6">
-                <div class="blog__item">
-                    <div class="blog__item__pic">
-                        <img src="img/blog/blog-2.jpg" alt="">
-                    </div>
-                    <div class="blog__item__text">
-                        <ul>
-                            <li><i class="fa fa-calendar-o"></i> May 4,2019</li>
-                            <li><i class="fa fa-comment-o"></i> 5</li>
-                        </ul>
-                        <h5><a href="#">6 ways to prepare breakfast for 30</a></h5>
-                        <p>Sed quia non numquam modi tempora indunt ut labore et dolore magnam aliquam quaerat </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-6">
-                <div class="blog__item">
-                    <div class="blog__item__pic">
-                        <img src="img/blog/blog-3.jpg" alt="">
-                    </div>
-                    <div class="blog__item__text">
-                        <ul>
-                            <li><i class="fa fa-calendar-o"></i> May 4,2019</li>
-                            <li><i class="fa fa-comment-o"></i> 5</li>
-                        </ul>
-                        <h5><a href="#">Visit the clean farm in the US</a></h5>
-                        <p>Sed quia non numquam modi tempora indunt ut labore et dolore magnam aliquam quaerat </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
