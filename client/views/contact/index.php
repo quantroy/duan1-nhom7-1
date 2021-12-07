c72c07a76dee84656638a27c68918a2dd6755e91
tranduykhanh2002 <khanhtdph16635@fpt.edu.vn>
2021-12-07 11:03:13 +0700

    hm

--- client/views/contact/index.php
+++ client/views/contact/index.php
@@ -75,18 +75,6 @@
             </div>
         </div>
         <form action="<?=BASE_URL . 'phan-hoi'?>" method="post">
-            <?php if (isset($_SESSION['false_number'])) {?>
-            <div class="alert alert-success d-flex align-items-center" role="alert">
-                <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:">
-                    <use xlink:href="#check-circle-fill" />
-                </svg>
-
-                <div>
-                    <?=$_SESSION['false_number']?>
-                </div>
-
-            </div>
-            <?php }?>
             <div class="row" style="height: 250px;">
                 <div class="col-6" style="height: 100%;">
                     <div class="col-lg-12 col-md-12">
